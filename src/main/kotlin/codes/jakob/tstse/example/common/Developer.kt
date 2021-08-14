b6617307c521e6da4f6dbbb1b0e577cbdac83812
Jakob Löhnertz <mail@jakob.codes>
2021-08-14 13:32:21 +0200

    Improve two 'common' classes

--- src/main/kotlin/codes/jakob/tstse/example/common/Developer.kt
+++ src/main/kotlin/codes/jakob/tstse/example/common/Developer.kt
@@ -13,4 +13,23 @@ data class Developer(
         get() {
             return assignment != null
         }
+
+    override fun toString(): String {
+        return "Developer(id=$id, name=$name, type=$type, emailAddress='$emailAddress', assigned=$assigned)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Developer
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
