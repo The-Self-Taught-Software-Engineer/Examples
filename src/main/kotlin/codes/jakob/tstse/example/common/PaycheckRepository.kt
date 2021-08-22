cae61c1d4b3e69a325e1d81fe037227d1043cd99
Jakob Löhnertz <mail@jakob.codes>
2021-08-22 12:56:59 +0200

    Scaffold an example

---
+++ src/main/kotlin/codes/jakob/tstse/example/common/PaycheckRepository.kt
@@ -0,0 +1,13 @@
+package codes.jakob.tstse.example.common
+
+import java.time.LocalDate
+
+class PaycheckRepository {
+    fun save(paychecks: List<Paycheck>) {
+        TODO("Not yet implemented")
+    }
+
+    fun saveFailed(developers: List<Developer>, paycheckPeriod: Pair<LocalDate, LocalDate>) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/codes/jakob/tstse/example/idiomatic/result/Result.kt
@@ -0,0 +1,89 @@
+@file:Suppress("unused")
+
+package codes.jakob.tstse.example.idiomatic.result
+
+import codes.jakob.tstse.example.common.*
+import codes.jakob.tstse.example.idiomatic.scopefunctions.DeveloperRepository
+import mu.KLogger
+import mu.KotlinLogging
+import java.time.Clock
+import java.time.LocalDate
+import java.time.Month
+import java.time.Year
+
+class PaycheckService(
+    private val humanResourcesService: HumanResourcesClient,
+    private val paycheckRepository: PaycheckRepository,
+    private val developerRepository: DeveloperRepository,
+    private val clock: Clock,
+) {
+    /**
+     * Returns an ordered list of [Paycheck]; sorted lexicographically by the [Name] of the [Developer].
+     */
+    fun generatePaychecks(developerType: DeveloperType, year: Year, month: Month): List<Paycheck> {
+        val today: LocalDate = LocalDate.now(clock)
+        val paycheckPeriod: Pair<LocalDate, LocalDate> = determinePeriod(year, month)
+
+        val (successfulPaychecks: List<Paycheck>, failedPaychecks: List<Developer>) =
+            developerRepository.findDevelopersByType(developerType)
+                .asSequence()
+                .map { dev ->
+                    val paycheckBuilder = Paycheck.Builder().apply {
+                        date = today
+                        period = paycheckPeriod
+                        developer = dev
+                    }
+                    dev to Result.success(paycheckBuilder)
+                }
+                .map { (dev, paycheckBuilder) ->
+                    dev to paycheckBuilder.mapCatching { paycheck ->
+                        paycheck.apply { hourlyRate = humanResourcesService.getHourlyRate(paycheck.developer!!) }
+                    }
+                }
+                .map { (dev, paycheckBuilder) ->
+                    dev to paycheckBuilder.mapCatching { paycheck ->
+                        paycheck.apply {
+                            hoursWorked = humanResourcesService.getHoursWorked(paycheck.developer!!, paycheckPeriod)
+                        }
+                    }
+                }
+                .map { (dev, paycheckBuilder) ->
+                    dev to paycheckBuilder.mapCatching { paycheck ->
+                        paycheck.build()
+                    }
+                }
+                .map { (dev, paycheckBuilder) ->
+                    dev to paycheckBuilder.onSuccess {
+                        logger.info {
+                            "Generation of ${Paycheck::class.simpleName} for '$dev' succeeded and persisted"
+                        }
+                    }
+                }
+                .map { (dev, paycheckBuilder) ->
+                    dev to paycheckBuilder.onFailure { exception ->
+                        logger.error(exception) {
+                            "Generation of ${Paycheck::class.simpleName} for '$dev' failed and persisted"
+                        }
+                    }
+                }
+                .partition { it.second.isSuccess }
+                .let { (success, failure) ->
+                    success.map { it.second.getOrThrow() } to failure.map { it.first }
+                }
+
+        paycheckRepository.save(successfulPaychecks)
+        paycheckRepository.saveFailed(failedPaychecks, paycheckPeriod)
+
+        return successfulPaychecks.sortedBy { it.developer.name }
+    }
+
+    private fun determinePeriod(year: Year, month: Month): Pair<LocalDate, LocalDate> {
+        val firstDayInPeriod: LocalDate = LocalDate.of(year.value, month, 1)
+        val lastDayInPeriod: LocalDate = firstDayInPeriod.withDayOfMonth(firstDayInPeriod.lengthOfMonth())
+        return firstDayInPeriod to lastDayInPeriod
+    }
+
+    companion object {
+        private val logger: KLogger = KotlinLogging.logger {}
+    }
+}
