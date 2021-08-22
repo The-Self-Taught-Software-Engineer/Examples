e4e3b8bd21c9f9428eacbb96200f0413122eb78e
Jakob Löhnertz <mail@jakob.codes>
2021-08-22 12:55:38 +0200

    Make the common 'Name' comparable

--- src/main/kotlin/codes/jakob/tstse/example/common/Name.kt
+++ src/main/kotlin/codes/jakob/tstse/example/common/Name.kt
@@ -3,11 +3,23 @@ package codes.jakob.tstse.example.common
 data class Name(
     val givenNames: List<String>,
     val familyName: String,
-) {
+) : Comparable<Name> {
     constructor(fullName: String) : this(
         givenNames = fullName.substringBeforeLast(' ').split(' '),
         familyName = fullName.substringAfterLast(' '),
     )
+
+    override fun compareTo(other: Name): Int {
+        val familyNameComparison: Int = this.familyName.compareTo(other.familyName)
+        return if (familyNameComparison != 0) {
+            familyNameComparison
+        } else {
+            this.givenNames.zip(other.givenNames)
+                .map { it.first.compareTo(it.second) }
+                .firstOrNull { it != 0 }
+                ?: 0
+        }
+    }
 
     override fun toString(): String {
         return "${givenNames.joinToString(" ")} $familyName"
